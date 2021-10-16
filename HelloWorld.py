69a1b7f20823783ba5528a5b4e6d8e495097a38d
Chloe Crozier <chloecrozier@83.165.21.198.tigernet.wifi.dyn.clemson.edu>
2021-10-16 15:49:07 -0400

    edited files

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,15 +6,7 @@ dictionary ={
     "SC": ["solar", "89%", "72%", "64%"],
     "CA": ["wind", "83%", "79%", "12%"]
 }
-      
-# Data to be written 
-# dictionary ={ 
-#   "id": "04", 
-#   "name": "sunil", 
-#   "department": "HR"
-# } 
 
-# arr = [dictionary, dictionary]
 
 from flask import jsonify
 @app.route('/numbers/')
@@ -23,4 +15,4 @@ def print_list():
 
 port = os.environ.get('PORT')
 
-app.run(port = port)
+app.run()
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 HelloWorld.py 
+web: gunicorn --bind 0.0.0.0:$PORT HelloWorld:app
